0b461f9b189cc5146e8f25b3a12d5ded26f06bb2
William Ye <williamye@Williams-MacBook-Pro.local>
2021-02-11 01:58:11 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import {map1, map2, map3} from './data.js';
+import React, {Component} from 'react';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(){
+    super();
+    this.state = {val1: "Jim Economou", lvl2: map1["Jim Economou"], 
+                  val2: "Daniel Clifford", lvl3: map2["Daniel Clifford"],
+                  val3: "Alain Laporte", lvl4: map3["Alain Laporte"],};
+    this.onChange1 = this.onChange1.bind(this);
+    this.onChange2 = this.onChange2.bind(this);
+    this.onChange3 = this.onChange3.bind(this);
+  }
+
+  onChange1(event){
+    let name = event.target.value;
+    console.log(name)
+    let data = map1[name];
+    this.setState({val1: name, lvl2: data});
+    console.log(this.state.lvl2)
+  }
+
+  onChange2(event){
+    let name = event.target.value;
+    console.log(name)
+    let data = map2[name];
+    this.setState({val2: name, lvl3: data});
+    console.log(this.state.lvl3)
+  }
+
+  onChange3(event){
+    let name = event.target.value;
+    console.log(name)
+    let data = map3[name];
+    this.setState({val3: name, lvl4: data});
+    console.log(this.state.lvl4)
+  }
+
+  getData(){
+    let data = this.state.lvl2;
+    let arr = [];
+    for (let i = 0; i < data.length; i++){
+      let a = <option value={data[i]}>{data[i]}</option>
+      arr.push(a)
+    }
+    return arr;
+  }
+
+  getData2(){
+    let d = this.state.lvl3;
+    let arr = [];
+    for (let i = 0; i < d.length; i++){
+      let a = <option value={d[i]}>{d[i]}</option>
+      arr.push(a)
+    }
+    return arr; 
+  }
+
+  getData3(){
+    let d = this.state.lvl4;
+    let arr = [];
+    for (let i = 0; i < d.length; i++){
+      let a = <option value={d[i]}>{d[i]}</option>
+      arr.push(a)
+    }
+    return arr; 
+  }
+  render(){
+    return (
+      <div>
+      <label> Director: 
+        <select value={this.state.val1} onChange={this.onChange1}>
+          <option value="Jim Economou" >Jim Economou</option>
+          <option value="Shahid Ibrahim">Shahid Ibrahim</option>
+          <option value="Wendy Hui">Wendy Hui</option>
+        </select>
+      </label>
+  
+  
+  
+  <label> Sr. Director: 
+    <select value={this.state.val2} onChange={this.onChange2}>
+      {this.getData()}
+    </select>
+  </label>
+
+  <label> lvl3: 
+    <select value={this.state.val3} onChange={this.onChange3}>
+      {this.getData2()}
+    </select>
+  </label>
+
+  <label> lvl4: 
+  <select>
+      {this.getData3()}
+    </select>
+  </label>
+  
+      </div>
+    
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const map1 = {"Wendy Hui": ["Daniel Clifford", "Satish Khandor", "Hassan Tannir", "Ryan Albanese"],
+            "Shahid Ibrahim": ["Carmine Guerrieri", "Houman Sadrzadeh", "Rob Jessup", "Julie Sutton"],
+            "Jim Economou": ["Null"]};
+
+export const map2 = {"Daniel Clifford": ["Alain Laporte", "Gurpreet Pathria", "Jorin Pook", "Keith Dorschner"],
+"Satish Khandor": ["Satish Khandor"], "Hassan Tannir" : ["Shashank Hegde"],
+"Ryan Albanese": ["Sandra Dalton", "Adrian Rodill", "TBD", "Marco Di Rienzo", "Betty Lee", "Pat Dam", "Bill Cherniatenski"]};
+
+export const map3 = {"Alain Laporte" : ["Betty Lee", "Krysanne Kin"],
+
+            "Gurpreet Pathria" : ["Giovanna Arcella", ],
+
+            "Jorin Pook" : ["Evelyn Anthonipillai", "Christine Callahan", "Jim McCrone"],
+
+            "Keith Dorschner" : ["Adrian Petryshyn"],
+
+            "Satish Khandor" : ["Satish Khandor", "Marcia Mastroyannopoulos"],
+
+            "Shashank Hegde" : ["Vivek Jagpal"],
+
+            "Sandra Dalton": ["Samanthi Bandara", "Patrick Mazzawi"],
+
+            "Adrian Rodill" : ["Marco Di Rienzo", "Dale Holloway"],
+
+            "Marco Di Rienzo" : ["Yoni Yacoubov"],
+
+            "Betty Lee" : ["Krysanne Kin", "Marcia Mastroyannopoulos", "Giancarlo Zambri"],
+
+            "Pat Dam" : ["Adrian Petryshyn"],
+
+            "Bill Cherniatenski" : ["Michelle Endall"]}
+
+
